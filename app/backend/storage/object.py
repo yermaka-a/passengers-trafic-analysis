18d56f435a4aba5c126c62945341fdf7389f7536
yermaka <ermak.080@mail.ru>
2026-02-25 20:54:45 +0800

    add get_all_objects to Objects storage

--- app/backend/storage/object.py
+++ app/backend/storage/object.py
@@ -1,12 +1,36 @@
 from ..schemas import ObjectCreate
 from sqlalchemy.orm import sessionmaker, Session
-
+from ..logger import logger
 from ..models import MapObject
+
+OP_CLASS_MSG = f"{__name__} class Objects "
 
 
 class Objects:
+
     def __init__(self, localSession: sessionmaker[Session]):
         self.localSession = localSession
+
+    def get_all_objects(self):
+        try:
+            with self.localSession() as ls:
+                objects = ls.query(MapObject).all()
+                print(objects)
+                return objects
+        except Exception as e:
+            op_method = "get_all_objects"
+            logger.error(OP_CLASS_MSG + op_method, extra={"error": e})
+            return None
+
+    def get(self, Id: str):
+        try:
+            with self.localSession() as ls:
+                obj = ls.get(MapObject, Id)
+                return obj
+        except Exception as e:
+            op_method = "get"
+            logger.error(OP_CLASS_MSG + op_method, extra={"error": e})
+            return None
 
     def create(self, obj: ObjectCreate):
         try:
@@ -17,20 +41,21 @@ class Objects:
                 new_obj = MapObject(
                     Id=options.Id,
                     color=options.color,
-                    custom_name=options.customName,
+                    custom_name=options.custom_name,
                     description=options.description,
                     fill=options.fill,
-                    fill_opacity=options.fillOpacity,
+                    fill_opacity=options.fill_opacity,
                     name=options.name,
-                    order=options.order,
                     stroke=options.stroke,
                     weight=options.weight,
                     latlng=latlng,
+                    obj_type=options.obj_type,
                 )
 
                 ls.add(new_obj)
                 ls.commit()
             return True
         except Exception as e:
-            print("exception:", e)
+            op_method = "create"
+            logger.error(OP_CLASS_MSG + op_method, extra={"error": e})
             return False
