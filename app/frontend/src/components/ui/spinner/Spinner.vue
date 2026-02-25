942f9f0f08811de3b499cf34e552de95f68479e0
yermaka <ermak.080@mail.ru>
2026-02-26 00:16:01 +0800

    add Spinner

---
+++ app/frontend/src/components/ui/spinner/Spinner.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Badge } from "@/components/ui/badge";
+import { Spinner } from "@/components/ui/spinner";
+</script>
+
+<template>
+  <div class="flex items-center gap-4 [--radius:1.2rem]">
+    <Badge variant="secondary">
+      <Spinner />
+      fdfd
+    </Badge>
+  </div>
+</template>
