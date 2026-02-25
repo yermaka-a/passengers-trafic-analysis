149313693706f5d9eb59470b735729d83200f9c4
yermaka <ermak.080@mail.ru>
2026-02-25 20:52:34 +0800

    add get_object get_all_objects to api

--- app/backend/crud/object.py
+++ app/backend/crud/object.py
@@ -1,6 +1,11 @@
-from ..schemas import ObjectCreate
-from pydantic import ValidationError
+from ..schemas import (
+    ObjectCreate,
+    AllObjectsResponse,
+    ObjectResponse,
+)
+from pydantic import TypeAdapter, ValidationError, UUID6
 from ..storage import Storage
+from ..logger import logger
 
 
 class ObjectController:
@@ -10,18 +15,41 @@ class ObjectController:
             raise Exception("storage.objects not created")
         self.objects = storage.objects
 
-    def create_object(self, data):
+    def get_object(self, data):
+        try:
+            id_validator = TypeAdapter(UUID6)
+            Id = id_validator.validate_python(data.Id)
+            if isinstance(Id, str):
+                obj = self.objects.get(Id)
+                if obj is not None:
+                    validated_obj = ObjectCreate.from_db(obj)
+                    return ObjectResponse(status="success", obj=validated_obj)
+                return {"status": "failed", "obj": obj}
+            raise Exception(f"incorrect Id: {Id}")
+        except Exception as e:
+            logger.error(e)
 
+    def create_object(self, data):
         try:
             obj_data = ObjectCreate(**data)
-            print(obj_data.model_dump_json())
             res = self.objects.create(obj_data)
             if res:
                 return {"status": "success", "message": obj_data.options.Id}
             return {"satus": "failed", "message": "data is not written"}
         except ValidationError as e:
-            print(e)
             return {"status": "failed", "message": e.json()}
+
+    def get_all_objects(self):
+        try:
+            objects = self.objects.get_all_objects()
+            if objects is not None:
+                validated_objects = [ObjectCreate.from_db(obj) for obj in objects]
+
+                return AllObjectsResponse(
+                    status="success", objects=validated_objects
+                ).model_dump(by_alias=True)
+        except Exception as e:
+            logger.error(e)
 
     def delete_object(self):
         pass
