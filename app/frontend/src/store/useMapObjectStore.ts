32b25cae12990d6bc9cc8d40ce43a49b6aff63eb
yermaka <ermak.080@mail.ru>
2026-02-25 20:51:38 +0800

    create methods loadAllObjectsFromDB getMapObjectByType createObjByTypeName

--- app/frontend/src/store/useMapObjectStore.ts
+++ app/frontend/src/store/useMapObjectStore.ts
@@ -2,23 +2,8 @@ import { defineStore } from "pinia";
 import * as L from "leaflet";
 import { v6 as uuidv6 } from "uuid";
 import { useApi } from "@/composables";
-
-declare module "leaflet" {
-  interface PolylineOptions {
-    Id: string;
-    order: number;
-    name: string;
-    CustomName?: string;
-    description?: string;
-  }
-  interface CircleMarkerOptions {
-    Id: string;
-    order: number;
-    name: string;
-    CustomName?: string;
-    description?: string;
-  }
-}
+import { useMapStore } from "@/store/useMapStore";
+import type { ObjectCreate } from "@/types";
 
 export class ObjectEditor {}
 export type Objects = L.Polygon | L.Polyline | L.CircleMarker;
@@ -29,20 +14,88 @@ const ObjectsTypes = [
   ["CircleMarker", "Маркер"],
   ["Edit", "Редактировать объект"],
 ] as const;
+export type ObjTypes = (typeof ObjectsTypes)[number][0];
+export type ObjNames = (typeof ObjectsTypes)[number][1];
 interface MapObjectStore {
-  Objects: Map<string, Objects>;
-  ObjectsCount: number;
+  Objects: Map<string, Objects> | null;
   ObjectsTypes: typeof ObjectsTypes;
   ChosenObjectType: (typeof ObjectsTypes)[number];
+  // Текущий создающийся объект карты
   MapObject: Objects | ObjectEditor | null;
   ClickedObjId: string | null;
 }
 
+const setId = (e: L.LeafletMouseEvent, store: MapObjectStore) => {
+  if (e.sourceTarget.options.Id) {
+    store.ClickedObjId = e.sourceTarget.options.Id;
+    console.log(e.sourceTarget.options.Id);
+  }
+};
+
+const getMapObjectByType = (
+  store: MapObjectStore,
+  ChosenObjectType: (typeof ObjectsTypes)[number],
+) => {
+  switch (ChosenObjectType[0]) {
+    case ObjectsTypes[0][0]: {
+      const polygon = new L.Polygon([], {
+        Id: uuidv6(),
+        objType: "Polygon",
+        name: "Полигон",
+      });
+      polygon.on("click", (e) => setId(e, store));
+      return polygon;
+    }
+    case ObjectsTypes[1][0]: {
+      const polyline = new L.Polyline([], {
+        Id: uuidv6(),
+        objType: "Polyline",
+        name: "Полилайн",
+      });
+      polyline.on("click", (e) => setId(e, store));
+      return polyline;
+    }
+    case ObjectsTypes[2][0]: {
+      const circleMarker = new L.CircleMarker([0, 0], {
+        Id: uuidv6(),
+        objType: "CircleMarker",
+        name: "Круговой маркер",
+      });
+      circleMarker.on("click", (e) => setId(e, store));
+      return circleMarker;
+    }
+    case ObjectsTypes[3][0]:
+      return new ObjectEditor();
+  }
+};
+
+const createObjByTypeName: Record<
+  Exclude<ObjTypes, "Edit">,
+  (objType: ObjectCreate, store: MapObjectStore) => Objects
+> = {
+  Polygon: (obj, store) => {
+    const polygon = new L.Polygon(obj.latlng, { ...obj.options });
+    polygon.on("click", (e) => setId(e, store));
+    return polygon;
+  },
+  Polyline: (obj, store): L.Polyline => {
+    const polyline = new L.Polyline(obj.latlng, { ...obj.options });
+    polyline.on("click", (e) => setId(e, store));
+    return polyline;
+  },
+  CircleMarker: (obj, store): L.CircleMarker => {
+    const circleMarker = new L.CircleMarker(obj.latlng as L.LatLngExpression, {
+      ...obj.options,
+    });
+    circleMarker.on("click", (e) => setId(e, store));
+    return circleMarker;
+  },
+};
+
 export const useMapObjectStore = defineStore("mapobjects", {
   state: (): MapObjectStore => {
     return {
-      Objects: new Map<string, Objects>(),
-      ObjectsCount: 0,
+      Objects: null,
       ObjectsTypes: ObjectsTypes,
       ChosenObjectType: ObjectsTypes[0],
       MapObject: null,
@@ -55,54 +108,45 @@ export const useMapObjectStore = defineStore("mapobjects", {
     getChosenObjectType: (state) => state.ChosenObjectType,
   },
   actions: {
+    async loadAllObjectsFromDB() {
+      if (!this.Objects) {
+        this.Objects = new Map();
+        const { getAllObjects } = useApi();
+        const objects = await getAllObjects();
+        if (objects) {
+          console.log("all objects from db:", objects);
+          const mapStore = useMapStore();
+          for (const obj of objects) {
+            if (mapStore.getMapRef) {
+              const newObj = createObjByTypeName[obj.options.objType](
+                obj,
+                this as unknown as MapObjectStore,
+              );
+              // TODO Переделать проверку на карту на хук
+              if (mapStore.mapInstance)
+                newObj.addTo(mapStore.mapInstance as L.Map);
+              this.Objects.set(newObj.options.Id, newObj);
+            }
+          }
+        }
+        return;
+      }
+      return this.Objects;
+    },
     setObjectType(option: (typeof ObjectsTypes)[number]) {
       this.$state.ChosenObjectType = option;
     },
     getObjectByChosenType() {
-      if (this.ChosenObjectType[0] !== "Edit") this.$state.ObjectsCount++;
-      const setId = (e: L.LeafletMouseEvent) => {
-        if (e.sourceTarget.options.Id) {
-          this.ClickedObjId = e.sourceTarget.options.Id;
-        }
-      };
-      const order = this.$state.ObjectsCount;
-      switch (this.ChosenObjectType[0]) {
-        case ObjectsTypes[0][0]: {
-          const polygon = new L.Polygon([], {
-            Id: uuidv6(),
-            order,
-            name: "Полигон",
-          });
-          polygon.on("click", setId);
-          return polygon;
-        }
-        case ObjectsTypes[1][0]: {
-          const polyline = new L.Polyline([], {
-            Id: uuidv6(),
-            order,
-            name: "Полилайн",
-          });
-          polyline.on("click", setId);
-          return polyline;
-        }
-        case ObjectsTypes[2][0]: {
-          const circleMarker = new L.CircleMarker([0, 0], {
-            Id: uuidv6(),
-            order,
-            name: "Круговой маркер",
-          });
-          circleMarker.on("click", setId);
-          return circleMarker;
-        }
-        case ObjectsTypes[3][0]:
-          return new ObjectEditor();
-      }
+      return getMapObjectByType(
+        this as unknown as MapObjectStore,
+        this.ChosenObjectType,
+      );
     },
     async setObject(Obj: Objects) {
       const Id = Obj.options.Id;
       const { createObject } = useApi();
       const objResponse = await createObject(Obj);
-      this.Objects.set(Id, Obj);
+      if (this.Objects) this.Objects.set(Id, Obj);
       return objResponse;
     },
     clearObject() {
