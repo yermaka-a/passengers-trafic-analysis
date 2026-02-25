9b161da25bea3ae472fd3a5f197b34a71f8ec88c
yermaka <ermak.080@mail.ru>
2026-02-25 23:41:12 +0800

    add update delete to storage

--- app/backend/storage/object.py
+++ app/backend/storage/object.py
@@ -1,3 +1,6 @@
+from pydantic import UUID6
+from sqlalchemy import delete
+
 from ..schemas import ObjectCreate
 from sqlalchemy.orm import sessionmaker, Session
 from ..logger import logger
@@ -22,10 +25,10 @@ class Objects:
             logger.error(OP_CLASS_MSG + op_method, extra={"error": e})
             return None
 
-    def get(self, Id: str):
+    def get(self, Id: UUID6):
         try:
             with self.localSession() as ls:
-                obj = ls.get(MapObject, Id)
+                obj = ls.get(MapObject, str(Id))
                 return obj
         except Exception as e:
             op_method = "get"
@@ -57,5 +60,45 @@ class Objects:
             return True
         except Exception as e:
             op_method = "create"
+            logger.error(OP_CLASS_MSG + op_method, extra={"error": e})
+            return False
+
+    def delete(self, Id: UUID6):
+        try:
+            with self.localSession() as ls:
+                ls.execute(delete(MapObject).where(MapObject.Id == str(Id)))
+                ls.commit()
+            return True
+        except Exception as e:
+            op_method = "delete"
+            logger.error(OP_CLASS_MSG + op_method, extra={"error": e})
+            return False
+
+    def update(self, obj: ObjectCreate):
+        try:
+            with self.localSession() as ls:
+
+                options = obj.options
+                latlng = [p.model_dump() for p in obj.latlng]
+                updated_obj = MapObject(
+                    Id=options.Id,
+                    color=options.color,
+                    custom_name=options.custom_name,
+                    description=options.description,
+                    fill=options.fill,
+                    fill_opacity=options.fill_opacity,
+                    name=options.name,
+                    stroke=options.stroke,
+                    weight=options.weight,
+                    latlng=latlng,
+                    obj_type=options.obj_type,
+                    dash_array=options.dash_array,
+                )
+
+                ls.merge(updated_obj)
+                ls.commit()
+            return True
+        except Exception as e:
+            op_method = "update"
             logger.error(OP_CLASS_MSG + op_method, extra={"error": e})
             return False
