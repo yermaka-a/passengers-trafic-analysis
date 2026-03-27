77739c9362a942f0a29080fb97fca5f8b6a1a4a5
yermaka <ermak.080@mail.ru>
2026-03-28 04:51:13 +0800

    cleanup: remove debug logging from updateViewState

--- app/frontend/src/store/useMapStore.ts
+++ app/frontend/src/store/useMapStore.ts
@@ -47,11 +47,8 @@ export const useMapStore = defineStore("mapstore", () => {
     if (mapInstance.value) {
       const { latitude, longitude, zoom, pitch, bearing } = viewState;
 
-      console.log("[useMapStore] updateViewState:", { latitude, longitude, zoom, animate });
-
       if (animate) {
         // Анимация полёта
-        console.log("[useMapStore] flyTo...");
         mapInstance.value.flyTo({
           center: [longitude, latitude],
           zoom,
@@ -62,7 +59,6 @@ export const useMapStore = defineStore("mapstore", () => {
         });
       } else {
         // Мгновенное перемещение
-        console.log("[useMapStore] easeTo...");
         mapInstance.value.easeTo({
           center: [longitude, latitude],
           zoom,
@@ -71,8 +67,6 @@ export const useMapStore = defineStore("mapstore", () => {
           duration: 500,
         });
       }
-    } else {
-      console.warn("[useMapStore] mapInstance is null!");
     }
   }
 
